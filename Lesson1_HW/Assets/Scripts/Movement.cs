037462bb850049182f8bc9b983e753c3d1d5044e
e.polishchuk <eugenia.polishchuk@gmail.com>
2020-11-22 10:31:40 +0200

    added lesson1 homework

---
+++ Lesson1_HW/Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10.0f;
+    private CharacterController _charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Motion(transform.forward);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Motion(transform.forward * (-1));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Motion(transform.right);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Motion(transform.right * (-1));
+        }    
+    }
+
+    private void Motion(Vector3 direction)
+    {
+        // Скорость игрока завиcит от скорости компьютера (кадровой частоты), 
+        // так как код движения зависит от частоты кадров игры и
+        // чем чаще вызывается функция Update тем быстрее двигается игрок.
+        // DeltaTime - время между кадрами. Умножение скорости на Time.deltaTime 
+        // делает скорость перемещений игрока независимой от скорости работы компьютера.
+        direction = direction * _speed *  Time.deltaTime;
+        _charController.Move(direction);
+    }
+}
